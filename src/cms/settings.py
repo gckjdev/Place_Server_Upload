a2a2abf5f34e19ed513a34d1616b9bb03c68be78
jianshao <wujianshao@gmail.com>
2011-05-18 23:14:33 +0800

    change db host to localhost

--- src/cms/settings.py
+++ src/cms/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'PlaceKS',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '192.168.1.101',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '9160',                      # Set to empty string for default. Not used with sqlite3.
         'SUPPORTS_TRANSACTIONS': False,
         'CASSANDRA_REPLICATION_FACTOR': 1,
